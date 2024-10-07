ac8c4c7929ecef48087ce042946a6d68a2795214
Hutomo Widjaja <flame.rainHbow@gmail.com>
2024-10-07 19:04:47 +0700

    test with stateless app

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text(
-            'I Am Rich',
-            style: TextStyle(color: Colors.white,),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-      body: const Center(
-        child: Image(image: AssetImage('images/IES.webp')),
-      )
-    ),
-  ));
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'I Am Rich',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: SafeArea(
+            child: Image(image: AssetImage('images/IES.webp')),
+          ),
+        ),
+        backgroundColor: Colors.blue,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          shape: const CircleBorder(),
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
